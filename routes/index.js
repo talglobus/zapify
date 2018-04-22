224e1ff5309f76e158939e97be610f2d0651188b
Tal Globus <talglobus@gmail.com>
2018-04-22 14:12:25 -0700

    Add website

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,10 @@ let router = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
 
+	// res.header('Access-Control-Allow-Origin', '*');
+	// res.render('index', { title: 'Zapify' });
 	res.header('Access-Control-Allow-Origin', '*');
-	res.render('index', { title: 'Zapify' });
+	res.sendFile('../public/index.html')
 });
 
 module.exports = router;
